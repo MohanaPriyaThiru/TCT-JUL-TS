e1ff5692eeeeb86803f339a8f1912ca2a523cd55
Aishu91122 <ice.sanju@gmail.com>
2026-09-03 13:17:36 +0530

    4th commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: [["html"], ["dot"], ["line"], ["list"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     testIdAttribute: "data-testid",
     viewport: null,
     launchOptions: { args: ["--start-maximized"], channel: "msedge" },
